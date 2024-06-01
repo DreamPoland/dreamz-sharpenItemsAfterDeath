ea5b7e79a35a7e86e55965092e10d90ca49a1543
aporofobia <afirmacion@proton.me>
2024-06-01 18:43:12 +0200

    made initial changes

--- plugin-core/src/main/kotlin/dev/aporofobia/order/controller/PlayerDeathController.kt
+++ plugin-core/src/main/kotlin/dev/aporofobia/order/controller/PlayerDeathController.kt
@@ -2,21 +2,29 @@ package dev.aporofobia.order.controller
 
 import dev.aporofobia.order.config.PluginConfig
 import eu.okaeri.injector.annotation.Inject
+import eu.okaeri.tasker.core.Tasker
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
+import java.time.Duration
 
 class PlayerDeathController @Inject constructor(
+    private val tasker: Tasker,
     private val pluginConfig: PluginConfig
 ) : Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val victim = event.entity
         if (victim !is Player) {
+            return
+        }
+
+        val killer = victim.killer
+        if (killer !is Player) {
             return
         }
 
@@ -27,10 +35,33 @@ class PlayerDeathController @Inject constructor(
         filteredDrops.shuffle()
 
         val percentage = this.pluginConfig.dropPercentage / 100.0
-        val itemsToDropCount = (filteredDrops.size * percentage).toInt()
+        val itemsForKillerCount = (filteredDrops.size * percentage).toInt()
 
-        event.drops.addAll(filteredDrops.take(itemsToDropCount))
+        // Items for the killer
+        val itemsForKiller = filteredDrops.take(itemsForKillerCount)
+        event.drops.addAll(itemsForKiller)
+
+        // Add ignored items back to the drops for the killer
         event.drops.addAll(drops.filter { it.type in this.pluginConfig.ignoredItems })
+
+        // Items for the victim
+        val itemsForVictim = filteredDrops.drop(itemsForKillerCount)
+        this.tasker.newDelayer(Duration.ofSeconds(2L))
+            .forceIf { victim.health > 0 }
+            .delayed {
+
+                for (item in itemsForVictim) {
+                    val remaining = victim.inventory.addItem(item).values
+                    // Drop any remaining items that couldn't fit in the victim's inventory
+                    for (remainingItem in remaining) {
+                        victim.world.dropItemNaturally(victim.location, remainingItem)
+                    }
+                }
+
+            }
+            .executeSync()
     }
+
+
 
 }
